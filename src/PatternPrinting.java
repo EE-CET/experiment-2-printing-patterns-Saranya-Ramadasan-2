450159003b0ea7a4051a0bb00e2209aece26378c
Saranya-Ramadasan <rsnyaramadasan04@gmail.com>
2026-04-03 00:08:00 +0530

    first commit

--- src/PatternPrinting.java
+++ src/PatternPrinting.java
@@ -1,5 +1,19 @@
 import java.util.Scanner;
 
 public class PatternPrinting {
+	public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        if (sc.hasNextInt()) {
+            int n = sc.nextInt();
+            for (int i = 0; i < n; i++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+            for (int i = 0; i < n; i++) {
+                System.out.println("*");
+            }
+        }
+        sc.close();
+    }
     
 }
